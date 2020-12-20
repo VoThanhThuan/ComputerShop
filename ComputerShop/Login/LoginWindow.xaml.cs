c67adf809f2873f8a4083f92dab26f08761946dd
Võ Thành Thuận <kocoten6969@gmail.com>
2020-12-20 22:07:46 +0700

    sửa đổi giao diện

--- ComputerShop/Login/LoginWindow.xaml.cs
+++ ComputerShop/Login/LoginWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Dashboard.Common;
 using Dashboard.Login;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -8,6 +9,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using Dashboard;
 using Dashboard.Common.ViewModel;
 using Dashboard.Data.Entities;
@@ -155,6 +157,12 @@ namespace DesignLogin
             {
                 AuthenticateUser();
             }
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri){ UseShellExecute = true});
+            e.Handled = true;
         }
     }
 
